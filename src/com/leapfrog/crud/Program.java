3bed98f2d12fda69f4ca974e283dc1347a50993d
Pradip Hamal <pradip.hamal31@gmail.com>
2018-01-13 21:27:10 +0545

    Changed GUI look and feel with that of Windows Look and Feel

--- src/com/leapfrog/crud/Program.java
+++ src/com/leapfrog/crud/Program.java
@@ -6,6 +6,10 @@
 package com.leapfrog.crud;
 
 import com.leapfrog.crud.ui.CourseUI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -17,9 +21,20 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        CourseUI courseUI=new CourseUI();
-        courseUI.setVisible(true);
+        try {
+            // TODO code application logic here
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            CourseUI courseUI=new CourseUI();
+            courseUI.setVisible(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Program.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Program.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Program.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Program.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
